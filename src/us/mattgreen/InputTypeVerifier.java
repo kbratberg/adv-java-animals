3d6d283f5ff3464c9e4c587ccf58838f35627761
kbratberg <kbratberg@my.wctc.edu>
2020-01-28 22:28:24 -0600

    updated UserInput Class,updated main

--- src/us/mattgreen/InputTypeVerifier.java
+++ src/us/mattgreen/InputTypeVerifier.java
@@ -1,4 +1,64 @@
 package us.mattgreen;
 
 public class InputTypeVerifier {
+    private String stringAnimal;
+    private String stringAge;
+    private String stringMiceKilled;
+    private boolean valid;
+    String stringKeepGoing;
+    int keepGoing;
+
+    public InputTypeVerifier(String stringAnimal, String isFriendly, String stringAge, String stringMiceKilled, String stringKeepGoing) {
+        this.stringAnimal = stringAnimal;
+        this.stringAge = stringAge;
+        this.stringMiceKilled = stringMiceKilled;
+        valid = true;
+    }
+
+    public InputTypeVerifier(String stringAge) {
+        this.stringAge = stringAge;
+    }
+
+
+
+    public int getAnimal() {
+        int animal = Integer.parseInt(stringAnimal);
+        return animal;
+    }
+
+    public void setStringAnimal(String stringAnimal) {
+        this.stringAnimal = stringAnimal;
+
+    }
+
+
+    public int getAge() {
+        int age = Integer.parseInt(stringAge);
+        return age;
+    }
+
+
+    public int getMousesKilled() {
+        int mousesKilled = Integer.parseInt(stringMiceKilled);
+        return mousesKilled;
+    }
+
+    public int getKeepGoing() {
+        return keepGoing;
+    }
+
+    public void setStringKeepGoing(String stringKeepGoing) {
+        this.stringKeepGoing = stringKeepGoing;
+        keepGoing = Integer.parseInt(stringKeepGoing);
+    }
+
+    public boolean isValid(String isFriendly)
+    {
+        if (!isFriendly.equalsIgnoreCase("y") || !isFriendly.equalsIgnoreCase("n"))
+        {
+            valid = false;
+        }
+        return valid;
+    }
+
 }
--- src/us/mattgreen/UserInput.java
+++ src/us/mattgreen/UserInput.java
@@ -2,73 +2,103 @@ package us.mattgreen;
 
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-    public class UserInput {
+
+public class UserInput {
 
         int animal;
+        String stringAnimal;
         String name = "";
         int mousesKilled = 0;
+        String stringMiceKilled;
         Boolean friendly = null;
         String isFriendly;
         int age=0;
+        String stringAge;
         Scanner input = new Scanner(System.in);
         ArrayList<Talkable> zoo;
+        boolean valid;
+        String stringKeepGoing;
+        int keepGoing;
 
         public UserInput(ArrayList<Talkable>zoo) {
            this.zoo = zoo;
 
         }
 
-        public void setUserInput(){
+        public void setUserInput() {
+            try {
+                    System.out.println("What type of animal would you like to create?\n" +
+                            "1. Cat\n2. Dog\n3. Person\n" +
+                            "Enter corresponding number> ");
 
-            System.out.println("What type of animal would you like to create?\n" +
-                    "1. Cat\n2. Dog\n3. Person\n" +
-                    "Enter corresponding number> ");
-            animal = input.nextInt();
-            if (animal == 1)
+                    stringAnimal = input.nextLine();
+                    animal = new InputTypeVerifier(stringAnimal).getAnimal();
+
+                    if (animal == 1) {
+                        input.nextLine();
+                        System.out.println("What is your cat's name>");
+                        name = input.nextLine();
+                        System.out.println("How many mice has your cat killed? ");
+                        stringMiceKilled = input.nextLine();
+                        mousesKilled = new InputTypeVerifier(stringMiceKilled).getMousesKilled();
+                        Cat newCat = new Cat(mousesKilled, name);
+                        zoo.add(newCat);
+                    } else if (animal == 2) {
+                        input.nextLine();
+                        System.out.println("What is your dog's name>");
+                        name = input.nextLine();
+                        System.out.println("Is your dog friendly Y/N? ");
+                        isFriendly = input.nextLine();
+                        valid = new InputTypeVerifier(isFriendly).isValid(isFriendly);
+                        while (valid = true) {
+                            if (isFriendly.equalsIgnoreCase("Y")) {
+                                friendly = true;
+                            } else if (isFriendly.equalsIgnoreCase("N")) {
+                                friendly = false;
+                            }
+                        }
+                        Dog newDog = new Dog(friendly, name);
+                        zoo.add(newDog);
+                    } else if (animal == 3) {
+                        input.nextLine();
+                        System.out.println("What is the person's name?");
+                        name = input.nextLine();
+                        System.out.println("What is the person's age?");
+                        stringAge = input.nextLine();
+                        age = new InputTypeVerifier(stringAge).getAge();
+                        Teacher newPerson = new Teacher(age, name);
+                        zoo.add(newPerson);
+                    }
+                   // System.out.println("To continue adding enter 1, otherwise to exit enter 0");
+                  //  stringKeepGoing = input.nextLine();
+                  //  keepGoing = new InputTypeVerifier(stringKeepGoing).getKeepGoing();
+
+            } catch (NumberFormatException e) {
+                System.out.println("Improper data type, try again");
+                setUserInput();
+            } catch (InputMismatchException e)
             {
-                input.nextLine();
-                System.out.println("What is your cat's name>");
-                name = input.nextLine();
-                System.out.println("How many mice has your cat killed? ");
-                mousesKilled = input.nextInt();
-                Cat newCat = new Cat(mousesKilled, name);
-                zoo.add(newCat);
-            }
-            else if (animal == 2)
+                System.out.println("You did not enter the correct response, try again");
+                setUserInput();
+            }catch (IllegalArgumentException e)
             {
-                input.nextLine();
-                System.out.println("What is your dog's name>");
-                name = input.nextLine();
-                System.out.println("Is your dog friendly Y/N? ");
-                isFriendly = input.nextLine();
-                if(isFriendly.equalsIgnoreCase("Y"))
-                {
-                    friendly = true;
-                }else if (isFriendly.equalsIgnoreCase("N"))
-                {
-                    friendly = false;
-                }
-                Dog newDog = new Dog(friendly, name);
-                zoo.add(newDog);
-            }
-            else if (animal == 3)
+                System.out.println("Sorry I don't understand, try again");
+                setUserInput();
+            }catch (Exception e)
             {
-                input.nextLine();
-                System.out.println("What is the person's name?");
-                name = input.nextLine();
-                System.out.println("What is the person's age?");
-                age = input.nextInt();
-                Teacher newPerson = new Teacher(age, name);
-                zoo.add(newPerson);
+                System.out.println("Something went wrong here, Please try again");
+                setUserInput();
             }
         }
 
 
-        public ArrayList<Talkable> getZoo() {
-            return zoo;
-        }
+            public ArrayList<Talkable> getZoo () {
+                return zoo;
+            }
+
     }
 
 
